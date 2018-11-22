4d817e97701bf85d2ce228c44932fcc400315651
dongeforever <dongeforever@apache.org>
2018-11-22 14:49:11 +0800

    Draft the interface

--- src/main/java/io/openmessaging/storage/Leger.java
+++ src/main/java/io/openmessaging/storage/Leger.java
@@ -1,20 +1,18 @@
-# openmessaging-storage
+package io.openmessaging.storage;
 
-A dead-simple unified ordered storage, act as the persistent layer of messaging and streaming system.
+public interface Leger {
 
-
-
-### append
     /**
      * Append data to the end of leger, and it should be assigned an index in numerical order strictly 0,1,2,3...
      * @param data
      * @return 0 means success, others means failure
      */
     int append(byte[] data);
-### get
+
     /**
      * Get data according to the index
      * @param index
      * @return
      */
     byte[] get(long index);
+}
